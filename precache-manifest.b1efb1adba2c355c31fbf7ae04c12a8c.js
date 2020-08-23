782c4a6268e13e57522cce64ccedca89222beb0b
shahidrugby <shahidrugby@aol.com>
2020-08-23 17:10:19 +0100

    Updates

---
+++ precache-manifest.b1efb1adba2c355c31fbf7ae04c12a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fbf8128ff0677188dd0989e298fa9a",
+    "url": "/react-app3/index.html"
+  },
+  {
+    "revision": "0362ca8979c85eabc1ac",
+    "url": "/react-app3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dccacaeb73d7604583e",
+    "url": "/react-app3/static/js/2.3e8e394a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app3/static/js/2.3e8e394a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0362ca8979c85eabc1ac",
+    "url": "/react-app3/static/js/main.efe1dcb3.chunk.js"
+  },
+  {
+    "revision": "482c1f0dd6414d028635",
+    "url": "/react-app3/static/js/runtime-main.780c5d9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app3/static/media/logo.5d5d9eef.svg"
+  }
+]);
